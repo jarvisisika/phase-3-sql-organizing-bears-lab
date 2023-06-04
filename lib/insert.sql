b7b700c10c3954d1180441d9ba4aba8f30f7bb39
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-06-05 00:58:10 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+-- Inserting bear records
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', TRUE),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', TRUE),
+  ('Tabitha', 7, 'F', 'Gray', 'Calm', TRUE),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Brave', TRUE),
+  ('Melissa', 4, 'F', 'Brown', 'Shy', TRUE),
+  ('Grinch', 2, 'M', 'Green', 'Mischievous', TRUE),
+  ('Wendy', 9, 'F', 'White', 'Gentle', TRUE),
+  ('unnamed', NULL, NULL, NULL, NULL, NULL);
